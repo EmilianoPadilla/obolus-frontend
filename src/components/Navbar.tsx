5a44fbd104316950e4fe3d6441e61a5faf556113
EmilianoPadilla <emilianopadillarobles@gmail.com>
2026-08-26 19:55:23 -0600

    fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -122,7 +122,7 @@ function Navbar() {
           )}
 
           {user ? (
-            <div className="flex flex-col gap-2 "> className=" text-sm text-gray-400 ">
+            <div className="flex flex-col gap-2"> 
               <span className="text-gray-300">Hi, {user.username}!</span>
               <Button
                 variant="outline"
